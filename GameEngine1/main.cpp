2dfba0a930a73af39f2611e63b86a904a1ff0577
Lucas Sindra <lucas.sindra@sap.com>
2018-12-06 21:44:37 -0200

    Initial Commit

---
+++ GameEngine1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameEngine1
+//
+//  Created by Sindra, Lucas on 06/12/18.
+//  Copyright © 2018 Sindra, Lucas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

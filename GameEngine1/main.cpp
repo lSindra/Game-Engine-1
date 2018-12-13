50c6a19a387de41780682efdf374157c45ec873c
Lucas Sindra <lucas.sindra@sap.com>
2018-12-13 11:16:28 -0200

    Added logical devices

--- GameEngine1/main.cpp
+++ GameEngine1/main.cpp
@@ -36,6 +36,8 @@ class Main {
     private: VkInstance instance;
     private: VkDebugUtilsMessengerEXT callback;
     private: VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    private: VkDevice device;
+    private: VkQueue graphicsQueue;
     
     static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
         VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
@@ -75,6 +77,41 @@ class Main {
         createInstance();
         setupDebugCallback();
         pickPhysicalDevice();
+        createLogicalDevice();
+    }
+    
+    private: void createLogicalDevice() {
+        QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+        
+        VkDeviceQueueCreateInfo queueCreateInfo = {};
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+        queueCreateInfo.queueCount = 1;
+        
+        float queuePriority = 1.0f;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+        
+        VkPhysicalDeviceFeatures deviceFeatures = {};
+        
+        VkDeviceCreateInfo createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        createInfo.pQueueCreateInfos = &queueCreateInfo;
+        createInfo.queueCreateInfoCount = queueCreateInfo.queueCount;
+        createInfo.pEnabledFeatures = &deviceFeatures;
+        createInfo.enabledExtensionCount = 0;
+        
+        if (enableValidationLayers) {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        } else {
+            createInfo.enabledLayerCount = 0;
+        }
+        
+        if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create logical device!");
+        }
+        
+        vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
     }
     
     private: void pickPhysicalDevice() {
@@ -101,12 +138,12 @@ class Main {
     }
     
     private: bool isDeviceSuitable(VkPhysicalDevice device) {
-        QueueFamilyIndices indices = findQueueIndices(device);
+        QueueFamilyIndices indices = findQueueFamilies(device);
         
         return indices.isComplete();
     }
     
-    private: QueueFamilyIndices findQueueIndices(VkPhysicalDevice device) {
+    private: QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
         QueueFamilyIndices indices;
         
         uint32_t queueFamilyCount = 0;
@@ -248,6 +285,7 @@ class Main {
             DestroyDebugUtilsMessengerEXT(instance, callback, nullptr);
         }
 
+        vkDestroyDevice(device, nullptr);
         vkDestroyInstance(instance, nullptr);
         
         glfwDestroyWindow(window);
